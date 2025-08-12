f4b11c0efb823efe818d90bf724346bb6ab46ea6
Ambarish Hazarnis <hazarnisa@gmail.com>
2025-08-12 15:42:17 -0700

    use yaml for configuration

---
+++ src/main/java/com/example/myapp/workflow/SurveyActivitiesImpl.java
@@ -0,0 +1,17 @@
+package com.example.myapp.workflow;
+
+import com.example.myapp.service.impl.SurveyService;
+
+public class SurveyActivitiesImpl implements SurveyActivities {
+
+    private final SurveyService surveyService;
+
+    public SurveyActivitiesImpl(SurveyService surveyService) {
+        this.surveyService = surveyService;
+    }
+
+    @Override
+    public boolean deleteQuestion(String surveyId, String questionId) {
+        return surveyService.deleteQuestionOfSurvey(surveyId, questionId);
+    }
+}
